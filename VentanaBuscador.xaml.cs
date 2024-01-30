2fc278f0d01137eb88a1c1843b654b472e79a3aa
Maria <maria8per@gmail.com>
2024-01-30 16:21:08 +0100

    initial

---
+++ VentanaBuscador.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelPereMaria
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaBuscador.xaml
+    /// </summary>
+    public partial class VentanaBuscador : Window
+    {
+        public VentanaBuscador()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void btnReservar1_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaReserva vtnReserva = new VentanaReserva();
+            vtnReserva.Show();
+        }
+
+        private void btnReservar2_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaReserva vtnReserva = new VentanaReserva();
+            vtnReserva.Show();
+        }
+
+        private void btnReservar3_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaReserva vtnReserva = new VentanaReserva();
+            vtnReserva.Show();
+        }
+
+        private void btnReservar4_Click(object sender, RoutedEventArgs e)
+        {
+            VentanaReserva vtnReserva = new VentanaReserva();
+            vtnReserva.Show();
+        }
+    }
+}
