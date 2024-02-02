34db504960fd7fc6352c8f989b77d63d62107493
Muhammad Hicho A Haidor <mh.haidor@gmail.com>
2024-02-02 17:44:48 +0100

    mvvm

--- views/VistaUserView.xaml.cs
+++ views/VistaUserView.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +23,19 @@ namespace HotelPereMaria.VistaUser
     /// </summary>
     public partial class VistaUserView : Window
     {
+        private VistaUserVM viewModel;
         public VistaUserView()
         {
             InitializeComponent();
+            viewModel = new();
+            DataContext = viewModel;
+            LoadReservations();
         }
+
+        private async void LoadReservations()
+        {
+           await viewModel.LoadReservations("correo1@example.com");
+        }
+        
     }
 }
