8c836f3badb69eee0a5ab3aa5bc1e02399966b78
Muhammad Hicho A Haidor <mh.haidor@gmail.com>
2024-02-06 14:33:53 +0100

    reservation details start

--- views/VentanaReserva.xaml.cs
+++ views/VentanaReserva.xaml.cs
@@ -1,7 +1,12 @@
 ﻿
 using HotelPereMaria.models;
 using HotelPereMaria.viewModels;
+using System.Globalization;
+using System;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
 
 namespace HotelPereMaria
 {
@@ -15,6 +20,38 @@ namespace HotelPereMaria
         {
             InitializeComponent();
             DataContext = new ReservaVM(currentUser);
+        }
+
+
+        private string ConvertTipoHabitacionToImage(int selectedIndex)
+        {
+            switch (selectedIndex)
+            {
+                case 0:
+                    return "../Images/rooms/estandar.jpg";
+                case 1:
+                    return "../Images/rooms/individual.jpg";
+                case 2:
+                    return "../Images/rooms/familiar.jpg";
+                case 3:
+                    return "../Images/rooms/deluxe.jpg";
+                case 4:
+                    return "../Images/rooms/suite.jpg";
+                default:
+                    return "../Images/rooms/estandar.jpg";
+            }
+        }
+
+        private void comboboxTipoHab_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int selectedRoom = comboboxTipoHab.SelectedIndex;
+            if (selectedRoom >= 0)
+            {
+                string imagePath = ConvertTipoHabitacionToImage(selectedRoom);
+
+                // Actualiza la imagen de la habitación
+                imageRoom.Source = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+            }
         }
     }
 }
