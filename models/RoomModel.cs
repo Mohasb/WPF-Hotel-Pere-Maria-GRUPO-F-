11c7dccfd5a98bf9068b0f04538a24b8744fffb7
Maria <maria8per@gmail.com>
2024-02-08 17:37:49 +0100

    cambios en VentanaBuscador y VentanaEditRoom

---
+++ models/RoomModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelPereMaria.models
+{
+    public class RoomModel : INotifyPropertyChanged
+    {
+        public int _room_number {  get; set; }
+        public string _type { get; set; }
+        public string _description { get; set; }
+        public double _price_per_night { get; set; }
+        public int _rate { get; set; }
+        public int _max_occupancy { get; set; }
+        public Boolean _isAvailable { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ viewModels/RoomVM.cs
@@ -0,0 +1,27 @@
+﻿using HotelPereMaria.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelPereMaria.viewModels
+{
+    public class RoomVM
+    {
+        private UserModel _user;
+        private int _room_number;
+        private string _type;
+        private string _description;
+        private double _price_per_night;
+        private int _rate;
+        private int _max_occupancy;
+        private Boolean _isAvailable;
+    }
+
+
+    public async Task LoadRooms (string userEmail)
+    {
+
+    }
+}
