25305b9ffccc59d3a532ea3caa81bbb25a74a383
Chitan <chitancai@gmail.com>
2024-02-14 23:08:52 +0100

    Update vista datagrid and listview

--- views/VistaDatagridView.xaml.cs
+++ views/VistaDatagridView.xaml.cs
@@ -26,7 +26,6 @@ namespace HotelPereMaria
             InitializeComponent();
             dgUsers.ItemsSource = LoadCollectionData();
             lvUsers.ItemsSource = LoadCollectionData();
-
         }
         private ObservableCollection<User> LoadCollectionData()
         {
@@ -114,12 +113,14 @@ namespace HotelPereMaria
         private void toggleButton_Checked(object sender, RoutedEventArgs e)
         {
             dgUsers.Visibility = toggleBtn.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            lblIconBtn.Content = "DataGrid";
             lvUsers.Visibility = toggleBtn.IsChecked == true ? Visibility.Collapsed : Visibility.Visible;
         }
 
         private void toggleButton_Unchecked(object sender, RoutedEventArgs e)
         {
             dgUsers.Visibility = toggleBtn.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            lblIconBtn.Content = "ListView";
             lvUsers.Visibility = toggleBtn.IsChecked == true ? Visibility.Collapsed : Visibility.Visible;
         }
     }
