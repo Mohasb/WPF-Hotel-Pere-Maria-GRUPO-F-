55b1cf473052a3dfc0c74f518790ab285e5012a1
Maria <maria8per@gmail.com>
2024-02-15 17:11:21 +0100

    ya traen rooms

--- models/RoomModel.cs
+++ models/RoomModel.cs
@@ -7,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace HotelPereMaria.models
 {
-    public class RoomModel : INotifyPropertyChanged
+    public class RoomModel
     {
-        public int _room_number {  get; set; }
-        public string _type { get; set; }
-        public string _description { get; set; }
-        public double _price_per_night { get; set; }
-        public int _rate { get; set; }
-        public int _max_occupancy { get; set; }
-        public Boolean _isAvailable { get; set; }
-        public event PropertyChangedEventHandler PropertyChanged;
+        public int RoomNumber { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public double Price_per_night { get; set; }
+        public int Rate { get; set; }
+        public int MaxOccupancy { get; set; }
+        public bool IsAvailable { get; set; }
+        public string Image { get; set; }
     }
 }
--- viewModels/RoomVM.cs
+++ viewModels/RoomVM.cs
@@ -1,10 +1,10 @@
 ﻿using HotelPereMaria.models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -12,22 +12,14 @@ namespace HotelPereMaria.viewModels
 {
     public class RoomVM
     {
-        //private UserModel _user;
-        //private int _room_number;
-        //private string _type;
-        //private string _description;
-        //private double _price_per_night;
-        //private int _rate;
-        //private int _max_occupancy;
-        //private Boolean _isAvailable;
-
+        public ObservableCollection<RoomModel> Rooms { get; } = new ObservableCollection<RoomModel>();
 
         public async Task LoadRooms(string userEmail = "correo1@example.com")
         {
             try
             {
                 string apiUrl = $"https://localhost/api/rooms/";
-                string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hcmlhQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsInVzZXIiOnsiX2lkIjoiNjViNDRmMTZhMDJiOTVmNTA4YzU4ZGZlIiwidXNlcl9uYW1lIjoiTWFyaWEiLCJlbWFpbCI6Im1hcmlhQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJFZzQy9RbU15UmdJOGJxdVphMVBGUnVybGQyUGdKRG1qM1VGa2oxNE9kN3BwbjVmUnBhbUJxIiwicm9sZSI6ImFkbWluIiwibmFtZSI6Ik1hcmlhIHByeiIsInBob25lIjo2MTUyMjM1MjgsInJlc2VydmF0aW9ucyI6W119LCJpYXQiOjE3MDc4NDI0MTYsImV4cCI6MTcwNzg3MTIxNn0.VWbC9EmgtjJfCL34fI64Gx0EpXGUKDBytSKxD8EpKfY";
+                string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hcmlhQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTcwODAxMTc5MCwiZXhwIjoxNzM5NTY5MzkwfQ.-ycLIEcnpQGrb-JnYA_RjRHy0qtJ_L3uxtSEIoQhp4E";
 
                 using (HttpClientHandler httpClientHandler = new HttpClientHandler())
                 {
@@ -35,23 +27,56 @@ namespace HotelPereMaria.viewModels
 
                     using (HttpClient httpClient = new HttpClient(httpClientHandler))
                     {
-
-                        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", token);
-
+                        httpClient.DefaultRequestHeaders.Add("Authorization", token);  //.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
                         HttpResponseMessage responseMessage = await httpClient.GetAsync(apiUrl);
 
                         if (responseMessage.IsSuccessStatusCode)
                         {
-                            string json = await responseMessage.Content.ReadAsStringAsync();
+                            var rooms = await responseMessage.Content.ReadAsStringAsync();
+                            foreach (var room in rooms)
+                            {
+                                //Rooms.Add(room);
+                            }
                         }
                     }
                 }
-            }catch (Exception ex)
+            }
+            catch (Exception ex)
             {
                 MessageBox.Show($"Error: {ex.Message}");
             }
         }
+        //public async Task LoadRooms(string userEmail = "correo1@example.com")
+        //{
+        //    try
+        //    {
+        //        string apiUrl = $"https://localhost/api/rooms/";
+        //        string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hcmlhQGdtYWlsLmNvbSIsInJvbGUiOiJhZG1pbiIsInVzZXIiOnsiX2lkIjoiNjViNDRmMTZhMDJiOTVmNTA4YzU4ZGZlIiwidXNlcl9uYW1lIjoiTWFyaWEiLCJlbWFpbCI6Im1hcmlhQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJFZzQy9RbU15UmdJOGJxdVphMVBGUnVybGQyUGdKRG1qM1VGa2oxNE9kN3BwbjVmUnBhbUJxIiwicm9sZSI6ImFkbWluIiwibmFtZSI6Ik1hcmlhIHByeiIsInBob25lIjo2MTUyMjM1MjgsInJlc2VydmF0aW9ucyI6W119LCJpYXQiOjE3MDc4NDI0MTYsImV4cCI6MTcwNzg3MTIxNn0.VWbC9EmgtjJfCL34fI64Gx0EpXGUKDBytSKxD8EpKfY";
+
+        //        using (HttpClientHandler httpClientHandler = new HttpClientHandler())
+        //        {
+        //            httpClientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+
+        //            using (HttpClient httpClient = new HttpClient(httpClientHandler))
+        //            {
+
+        //                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Authorization", token);
+
+
+        //                HttpResponseMessage responseMessage = await httpClient.GetAsync(apiUrl);
+
+        //                if (responseMessage.IsSuccessStatusCode)
+        //                {
+        //                    string json = await responseMessage.Content.ReadAsStringAsync();
+        //                }
+        //            }
+        //        }
+        //    }catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Error: {ex.Message}");
+        //    }
+        //}
 
     }
 
